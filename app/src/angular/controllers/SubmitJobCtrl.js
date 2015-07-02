0f44d18f2ebc49bc5c8ae5c325086d018647fc45
leachuk <stew.leach@gmail.com>
2015-07-02 23:03:07 +1000

    updating angular material dependency versions. This fixed TypeError which were being thrown

--- app/src/angular/controllers/SubmitJobCtrl.js
+++ app/src/angular/controllers/SubmitJobCtrl.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('recruitUnitApp')
-  .controller('SubmitJobCtrl', function ($scope, $http) {
+  .controller('SubmitJobCtrl', ['$scope', '$http', function ($scope, $http) {
 	var self = this;
   	self.tags = ['foo', 'bar'];
+  	console.log("tags:" + self.tags);
 
-  	$scope.submitJob = function(){
-		console.log("submitJob");
-  	};
+  // 	$scope.submitJob = function(){
+		// console.log("submitJob");
+  // 	};
 	
-  });
+  }]);
