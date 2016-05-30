e7b1d50a7d9ad302be508cf09d11b60fb9bae388
stewart leach <stewartleach@Naya-Las-MacBook-Pro.local>
2016-05-30 21:40:49 +1000

    fixing up authentication so form can be submitted

--- app/src/angular/components/formSubmit/FormSubmitController.js
+++ app/src/angular/components/formSubmit/FormSubmitController.js
@@ -32,25 +32,19 @@
       "submittedBy" : ""
     };
 
-    var checkAuth = function(username, password){
-      var authCookie = $cookies.get("writeon.authtoken"); //put cookie name into config var
-      return typeof authCookie != 'undefined' ? authCookie : loomApi.User.signInUser(username, password).then(angular.bind(this, function(result){
-        $cookies.put("writeon.authtoken", result.token);
-        this.authToken = result.token; //required angular.bind to enable setting of scope variable within promise
-      }));
-    };
-    this.authToken = checkAuth("writeonmvpstep1-1@test.com", "12345678");
-
     Controller.prototype.submitJobToCandidate = function(){
       console.log("in submitJobToCandidate");
+      var authToken = $cookies.get("writeon.authtoken");
 
-      if(submitJobFromRecruiter.checkValidity()){ //submitJobFromRecruiter is form name
+      //ToDo: handle if authToken is expired/not available. Redirect to login page?
+      if(submitJobFromRecruiter.checkValidity() && typeof authToken != 'undefined'){ //submitJobFromRecruiter is form name
         console.log("model:");
         console.log(this.article);
 
-        loomApi.Article.saveArticle(this.article, 'server/services/recruitunit/articles/recruitUnitContentService.controller.js', this.authToken).then(angular.bind(this,function(saveResult){
+        loomApi.Article.saveArticle(this.article, 'server/services/recruitunit/articles/recruitUnitContentService.controller.js', authToken).then(angular.bind(this,function(saveResult){
           console.log("result:");
           console.log(saveResult);
+          //ToDo: redirect to users homepage after success
           saveResult.success ? this.submitmessage = "Success message" : this.submitmessage = "Error. " + saveResult.message;
         }));
       }
