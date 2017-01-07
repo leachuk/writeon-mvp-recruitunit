fd22e8b6dcce9fda5356fda275ddc3c53b4e9a77
stewart leach <stewart.leach@originenergy.com.au>
2017-01-07 14:33:56 +1100

    adding test form url copy dialog and clipboard action

--- app/src/angular/components/userLanding/UserLandingController.js
+++ app/src/angular/components/userLanding/UserLandingController.js
@@ -44,6 +44,7 @@
     this.comparisonFormTemplate = "src/angular/components/userLanding/requireComparisonFormDialog.html";
     this.enableContactMeTemplate = "src/angular/components/userLanding/enableContactMeDialog.html";
     this.disableContactMeTemplate = "src/angular/components/userLanding/disableContactMeDialog.html";
+    this.unitTestFormTemplate = "src/angular/components/userLanding/unitTestFormDialog.html";
     this._mdPanel = $mdPanel;
     this.openFrom = 'button';
     this.closeTo = 'button';
@@ -138,7 +139,7 @@
   }
 
   Controller.prototype.showPanel = function() {
-    console.log("in showPanel()");
+    console.log("in showPanel");
     var panelPosition = this._mdPanel.newPanelPosition()
         .absolute()
         .center();
@@ -171,7 +172,7 @@
   }
 
   Controller.prototype.toggleContactMeDialog = function($event, id, displayDevEmail, repeatScope) {
-    console.log("in toggleContactMeDialog()");
+    console.log("in toggleContactMeDialog");
 
     var panelPosition = this._mdPanel.newPanelPosition()
         .absolute()
@@ -196,6 +197,39 @@
       position: panelPosition,
       animation: panelAnimation,
       templateUrl: this.enableContactMeTemplate,
+      hasBackdrop: true,
+      panelClass: 'demo-dialog-example',
+      zIndex: 150,
+      clickOutsideToClose: true,
+      escapeToClose: true,
+      focusOnOpen: true
+    }
+
+    this._mdPanel.open(config);
+  }
+
+  Controller.prototype.toggleUnitTestFormDialog = function($event, unitTestFormUrl) {
+    console.log("in toggleUnitTestFormDialog");
+
+    var panelPosition = this._mdPanel.newPanelPosition()
+        .absolute()
+        .center();
+    var panelAnimation = this._mdPanel.newPanelAnimation();
+    panelAnimation.openFrom({top:0, left:0});
+    panelAnimation.closeTo({top:document.documentElement.clientHeight, left:0});
+    panelAnimation.withAnimation(this._mdPanel.animation.SCALE);
+
+    var config = {
+      attachTo: angular.element(document.body),
+      controller: DialogController,
+      controllerAs: 'unitTestFormDialog',
+      locals: {
+        'testFormUrl' : unitTestFormUrl
+      },
+      bindToController: true,
+      position: panelPosition,
+      animation: panelAnimation,
+      templateUrl: this.unitTestFormTemplate,
       hasBackdrop: true,
       panelClass: 'demo-dialog-example',
       zIndex: 150,
@@ -269,6 +303,43 @@
       }));
 
       panelRef.close()
+    }
+
+    DialogController.prototype.copyUnitTestFormUrl = function(testFormUrl) {
+      console.log("copyUnitTestFormUrl testFormUrl:" + testFormUrl);
+
+      var panelRef = this._mdPanelRef;
+
+      // Copies a string to the clipboard. Must be called from within an
+      // event handler such as click. May return false if it failed, but
+      // this is not always possible. Browser support for Chrome 43+,
+      // Firefox 42+, Safari 10+, Edge and IE 10+.
+      // IE: The clipboard feature may be disabled by an administrator. By
+      // default a prompt is shown the first time the clipboard is
+      // used (per session).
+
+      var text = testFormUrl;
+      if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text);
+
+      } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+          return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        } catch (ex) {
+          console.warn("Copy to clipboard failed.", ex);
+          return false;
+        } finally {
+          document.body.removeChild(textarea);
+          panelRef.close()
+        }
+      }
+
     }
   }
 
